21b86ba22f24de8cd30505000cd97bad367291b4
Kirill <kirill.novikov@gmail.com>
2023-05-19 15:41:23 +0300

    Add signin and signup pages and stiles

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,18 @@
 ﻿function Login() {
-  return <div>Login</div>;
+  return (
+    <main className='main page__main page__main_form'>
+      <h1 className='main__title'>Вход</h1>
+      <form className='main__form'>
+        <input className='main__input' type='email' placeholder='Email'></input>
+        <input
+          className='main__input'
+          type='password'
+          placeholder='Пароль'
+        ></input>
+        <button className='main__button'>Войти</button>
+      </form>
+    </main>
+  );
 }
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,21 @@
 ﻿function Register() {
-  return <div>Register</div>;
+  return (
+    <main className='main page__main page__main_form'>
+      <h1 className='main__title'>Регистрация</h1>
+      <form className='main__form'>
+        <input className='main__input' type='email' placeholder='Email'></input>
+        <input
+          className='main__input'
+          type='password'
+          placeholder='Пароль'
+        ></input>
+        <button className='main__button'>Зарегистрироваться</button>
+        <a className='main__link' href='/sign-in'>
+          Уже зарегистрированы? Войти
+        </a>
+      </form>
+    </main>
+  );
 }
 
 export default Register;
